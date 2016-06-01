0f4dca3de318ce05afa2bb7919cc1d36af79dea5
Evan Felix <evan.felix@pnnl.gov>
2016-06-01 11:46:09 -0700

    tweak for EMSL version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alexey.noskov@gmail.com'
 license          'MIT'
 description      'Installs Motion, a software motion detector.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 source_url       'https://github.com/alno/chef-motion' if respond_to?(:source_url)
 issues_url       'https://github.com/alno/chef-motion' if respond_to?(:issues_url)
 
